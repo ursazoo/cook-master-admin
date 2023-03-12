a47d89a434b2cd996444ddabd854271db9c50fdc
秦锋 <iamzhoufei@gmail.com>
2023-03-12 23:57:51 +0800

    chore: 更新项目配置

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+
+import * as React from "react";
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            "em-emoji": React.DetailedHTMLProps<
+                React.HTMLAttributes<HTMLElement>,
+                HTMLElement
+            > & {
+                id?: string;
+                shortcodes?: string;
+                native?: string;
+                size?: string | number;
+                fallback?: string;
+                set?: "native" | "apple" | "facebook" | "google" | "twitter";
+                skin?: string | number;
+            };
+        }
+    }
+}
