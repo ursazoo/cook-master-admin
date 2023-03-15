b2286555c171606ae4987b13adcfb761cf5ac41a
秦锋 <iamzhoufei@gmail.com>
2023-03-15 23:57:16 +0800

    init: 项目初始化

---
+++ config-overrides.js
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const {
+  override,
+  addWebpackModuleRule,
+  addWebpackPlugin,
+  addWebpackAlias,
+  overrideDevServer
+} = require('customize-cra');
+const ArcoWebpackPlugin = require('@arco-plugins/webpack-react');
+const addLessLoader = require('customize-cra-less-loader');
+const setting = require('./src/settings.json');
+
+const addDevServerConfig = () => (config) => {
+  // 在这里写你自己的配置
+  return {
+    ...config,
+    proxy: {
+      '/api': 'http://localhost:3000',
+      changeOrigin: true
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
+};
+
+module.exports = {
+  devServer: overrideDevServer(addDevServerConfig()),
+  webpack: override(
+    addLessLoader({
+      lessLoaderOptions: {
+        lessOptions: {}
+      }
+    }),
+    addWebpackModuleRule({
+      test: /\.svg$/,
+      loader: '@svgr/webpack'
+    }),
+    addWebpackPlugin(
+      new ArcoWebpackPlugin({
+        theme: '@arco-themes/react-arco-pro',
+        modifyVars: {
+          'arcoblue-6': setting.themeColor
+        }
+      })
+    ),
+    addWebpackAlias({
+      '@': path.resolve(__dirname, 'src')
+    })
+  )
+};
